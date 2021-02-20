eb07c91e12b79079ba5be6c06aa6c1ff5b87626b
Dean321 <parvathi.saxena@gmail.com>
2021-02-20 17:01:06 +0530

    Update bob.js

--- pro-27/bob.js
+++ pro-27/bob.js
@@ -1,11 +1,11 @@
 class Bob{
     constructor(x,y,w,h){
-        this.w=40
+        this.w=20
         this.h=40
     this.body=Matter.Bodies.circle(x,y,this.w,{
       
-        restitution:0,
-        friction:1,
+        restitution:1,
+        friction:0,
         density:1
 
        })
@@ -13,13 +13,13 @@ class Bob{
     this.img=loadImage("mar.png")
     }
     display(){
-    push()
-    imageMode(CENTER) 
-    translate(this.body.position.x,this.body.position.y)
-    rotate(this.body.angle)
-    image(this.img,0,0,this.w,this.w) 
-    ellipse(RADIUS)
-    ellipse(0,0,this.w)
-    pop()
+        push()
+        imageMode(CENTER) 
+        translate(this.body.position.x,this.body.position.y)
+        rotate(this.body.angle)
+        image(this.img,0,0,this.w*2,this.w*2) 
+        //ellipseMode(RADIUS)
+        //ellipse(0,0,this.w)
+        pop()
     }
     }
